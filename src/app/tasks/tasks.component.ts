d8e8c079c9e07ebf9ce39eb2a56cca64e113c59e
ammwangi <elijah.mwangi@kamsware.com>
2025-06-24 14:02:09 +0300

    use of dialogs and event emitters

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,17 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { DUMMY_TASKS } from '../dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({}) userId! : string;
   @Input({}) name!: string;
+  isAddingTask = false;
 
   tasks = DUMMY_TASKS;
 
@@ -21,5 +23,13 @@ export class TasksComponent {
  
   onCompleteTask(id: string ) {
      this.tasks = this.tasks.filter((task) => task.id !== id);
+  } 
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  } 
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }
