975ef690a0aad4638dcfcc3f6d5fda11810dedcb
ammwangi <elijah.mwangi@kamsware.com>
2025-06-24 14:47:59 +0300

    two way b inding

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,35 +1,48 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
+import { TaskComponent } from './task/task.component';
 import { DUMMY_TASKS } from '../dummy-tasks';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input({}) userId! : string;
+  @Input({}) userId!: string;
   @Input({}) name!: string;
   isAddingTask = false;
 
   tasks = DUMMY_TASKS;
 
-  get selectedUserTasks () {
-    return this.tasks.filter((task) => task.userId === this.userId)
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
   }
- 
-  onCompleteTask(id: string ) {
-     this.tasks = this.tasks.filter((task) => task.id !== id);
-  } 
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
 
   onStartAddTask() {
     this.isAddingTask = true;
-  } 
+  }
 
   onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+      userId: this.userId,
+    }); 
+
     this.isAddingTask = false;
   }
 }
