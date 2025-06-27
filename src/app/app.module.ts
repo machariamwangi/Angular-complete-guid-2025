1683c3d87f415a61b8a0e048ed8d6447f0915aad
ammwangi <elijah.mwangi@kamsware.com>
2025-06-27 12:41:16 +0300

    working with nested modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderCompenent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderCompenent,
     UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule, SharedModule],
+  imports: [BrowserModule, SharedModule, TasksModule],
 })
 export class AppModule {}
